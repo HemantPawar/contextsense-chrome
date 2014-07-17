305a6804d7c51db6148e496748faefcf254f208c
Hemant Pawar <hemant@hemantpawar.com>
2014-07-17 19:54:03 +0530

    added loading progress while making request

--- source/js/script.js
+++ source/js/script.js
@@ -7,9 +7,14 @@ chrome.tabs.getSelected( function(tab){
     req.onload = function(){
         doc.documentElement.innerHTML = req.responseText;
 
+        // set contextsense block
         document.getElementById("result").innerHTML = doc.getElementsByClassName("result")[0].innerHTML;
         document.getElementById("value").style.width = doc.getElementsByClassName("value")[0].style.width;
         document.getElementById("report").addEventListener('click', function(){window.open(url)});
+
+        // display contextsense block
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("contextsense").style.display = "block";
     }
     req.send(null);
 
